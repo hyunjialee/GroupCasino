3fd5cc98044036b635c07341957227176ca4254b
nathan <nathan.guardado@yahoo.com>
2023-11-13 21:52:47 -0500

    Card Class Pt2

--- src/main/java/com/github/zipcodewilmington/casino/games/GameUtils/CardClass/Deck.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/GameUtils/CardClass/Deck.java
@@ -1,2 +1,31 @@
-package com.github.zipcodewilmington.casino.games.GameUtils.CardClass;public class Deck {
+package com.github.zipcodewilmington.casino.games.GameUtils.CardClass;
+
+import java.util.Collections;
+import java.util.Stack;
+
+public class Deck {public Stack<Cards> deckOfCards = new Stack<>();
+
+    public void shuffle(){
+        Collections.shuffle(deckOfCards);
+    }
+
+    public Cards draw(){
+        return deckOfCards.pop();
+    }
+
+    public Cards flip(){
+        return deckOfCards.peek();
+    }
+
+    // needs 1-13 of each suit
+    // assign each index to a specific card,
+    // e.g. 0-12 = spades; 13-25 = clubs; 26-38 = hearts; 39-51 = diamonds
+
+    public Deck() {
+        for (Suit suit : Suit.values()) {
+            for (NumberValue numberValue : NumberValue.values()) {
+                deckOfCards.push(new Cards(suit, numberValue));
+            }
+        }
+    }
 }
