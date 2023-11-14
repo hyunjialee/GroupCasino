507bd723c123de3493221c4793dfd1f9c5bfbd54
nathan <nathan.guardado@yahoo.com>
2023-11-13 21:29:34 -0500

    Card Class Pt1

--- src/main/java/com/github/zipcodewilmington/casino/games/GameUtils/CardClass/Cards.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/GameUtils/CardClass/Cards.java
@@ -1,4 +1,33 @@
 package com.github.zipcodewilmington.casino.games.GameUtils.CardClass;
 
 public class Cards {
+
+    private Suit suit;
+    private NumberValue numberValue;
+
+    public Cards(Suit suit, NumberValue numberValue) {
+        this.suit = suit;
+        this.numberValue = numberValue;
+
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public void setSuit(Suit suit) {
+        this.suit = suit;
+    }
+
+    public NumberValue getNumberValue() {
+        return numberValue;
+    }
+
+    public void setNumberValue(NumberValue numberValue) {
+        this.numberValue = numberValue;
+    }
+
+    public Cards drawCards() {
+        return null;
+    }
 }
