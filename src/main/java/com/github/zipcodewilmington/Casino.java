530b08bb291c5c3b0f21d71ee4052f4536fb5279
hyunjialee <hyjialee@gmail.com>
2023-11-15 12:48:36 -0500

    fix roll dice

--- src/main/java/com/github/zipcodewilmington/Casino.java
+++ src/main/java/com/github/zipcodewilmington/Casino.java
@@ -66,7 +66,7 @@ public class Casino implements Runnable {
         return console.getStringInput(new StringBuilder()
                 .append("Welcome to the Game Selection Dashboard!")
                 .append("\nFrom here, you can select any of the following options:")
-                .append("\n\t[ SLOTS ], [ BLACKJACK ], [ BINGO ], [ CHOHAN ], [ CRAPS ], [ WAR ]")
+                .append("\n\t[ slots ], [ blackjack ], [ bingo ], [ chohan ], [ craps ], [ war ]")
                 .toString());
     }
 
