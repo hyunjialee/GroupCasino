cdcace266d93e096cf14a692cf6040695fcab30c
nathan <nathan.guardado@yahoo.com>
2023-11-14 22:59:53 -0500

    Various Tests

--- src/main/java/com/github/zipcodewilmington/casino/games/GameUtils/CardClass/Cards.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/GameUtils/CardClass/Cards.java
@@ -28,26 +28,4 @@ public class Cards {
     public void setNumberValue(NumberValue numberValue) {
         this.numberValue = numberValue;
     }
-
-    public Cards drawCards() {
-        return null;
-    }
-    @Override
-    public String toString(){
-        return suit + "-" + numberValue;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Cards card = (Cards) o;
-        return suit == card.suit &&
-                numberValue == card.numberValue;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(suit, numberValue);
-    }
 }
