7ee5205befdbec14e4d1027867d0b40fd1a2afd6
hyunjialee <hyjialee@gmail.com>
2023-11-15 23:22:20 -0500

    test stubs

--- src/test/java/com/github/zipcodewilmington/casinoAccountTests/CasinoAccountTest.java
+++ src/test/java/com/github/zipcodewilmington/casinoAccountTests/CasinoAccountTest.java
@@ -1,4 +1,36 @@
 package com.github.zipcodewilmington.casinoAccountTests;
 
+import org.junit.jupiter.api.Test;
+
 public class CasinoAccountTest {
+
+    @Test
+    public void testGetName() {
+
+    }
+
+    @Test
+    public void testSetName() {
+
+    }
+
+    @Test
+    public void testGetPassword() {
+
+    }
+
+    @Test
+    public void testSetPassword() {
+
+    }
+
+    @Test
+    public void testGetBalance() {
+
+    }
+
+    @Test
+    public void testSetBalance() {
+
+    }
 }
--- src/test/java/com/github/zipcodewilmington/dicetest/ChohangameTest.java
+++ src/test/java/com/github/zipcodewilmington/dicetest/ChohangameTest.java
@@ -1,4 +1,15 @@
 package com.github.zipcodewilmington.dicetest;
 
+import org.junit.jupiter.api.Test;
+
 public class ChohangameTest {
+
+    @Test
+    public void testAdd(){
+
+    }
+
+    @Test
+    public void testAdd(){
+    }
 }
